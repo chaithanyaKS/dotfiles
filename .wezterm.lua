40597f882d61c0a1eb2e2a85e17dbaee4081981e
chaithanyaKS <46883790+chaithanyaKS@users.noreply.github.com>
2026-07-02 22:17:15 +0530

    Refactor WezTerm configuration for clarity and updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -133,81 +133,45 @@ end)
 
 ---[ BASE KEYBINDINGS
 config.keys = {
-	-- Send Ctrl+a to terminal when pressed twice
-	{ key = "a",   mods = "LEADER|CTRL",  action = act.SendString("\x01") },
-
-	-- Doom Mode trigger
-	{ key = 'w',   mods = 'LEADER',       action = act.ActivateKeyTable { name = 'window_mode', one_shot = true } },
-	{ key = 'Tab', mods = 'LEADER',       action = act.ActivateKeyTable { name = 'workspace_mode', one_shot = true } },
-
 	-- Flat binding
-	{ key = '"',   mods = "LEADER|SHIFT", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
-	{ key = "%",   mods = "LEADER|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
-	{ key = "o",   mods = "LEADER",       action = act.TogglePaneZoomState },
-	{ key = "z",   mods = "LEADER",       action = act.TogglePaneZoomState },
-	{ key = "c",   mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain") },
-	{ key = "n",   mods = "LEADER|CTRL",  action = act.ActivateTabRelative(1) },
-	{ key = "p",   mods = "LEADER|CTRL",  action = act.ActivateTabRelative(-1) },
-	{ key = 's',   mods = 'LEADER',       action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|TABS|WORKSPACES' }, },
-	{ key = "f",   mods = "LEADER|CTRL",  action = wezterm.action_callback(project_launcher), },
+	{ key = '"', mods = "LEADER|SHIFT", action = act.SplitVertical { domain = "CurrentPaneDomain" } },
+	{ key = "%", mods = "LEADER|SHIFT", action = act.SplitHorizontal { domain = "CurrentPaneDomain" } },
+	{ key = "o", mods = "LEADER",       action = act.TogglePaneZoomState },
+	{ key = "z", mods = "LEADER",       action = act.TogglePaneZoomState },
+	{ key = "c", mods = "LEADER",       action = act.SpawnTab("CurrentPaneDomain") },
+	{ key = "n", mods = "LEADER|CTRL",  action = act.ActivateTabRelative(1) },
+	{ key = "p", mods = "LEADER|CTRL",  action = act.ActivateTabRelative(-1) },
+	{ key = 's', mods = 'LEADER',       action = wezterm.action.ShowLauncherArgs { flags = 'FUZZY|TABS|WORKSPACES' }, },
+	{ key = "f", mods = "LEADER|CTRL",  action = wezterm.action_callback(project_launcher), },
+	{ key = "[", mods = "LEADER",       action = wezterm.action.ActivateCopyMode, },
 
 	-- Navigation
-	{ key = "h",   mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
-	{ key = "j",   mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
-	{ key = "k",   mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
-	{ key = "l",   mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
+	{ key = "h", mods = "LEADER",       action = act.ActivatePaneDirection("Left") },
+	{ key = "j", mods = "LEADER",       action = act.ActivatePaneDirection("Down") },
+	{ key = "k", mods = "LEADER",       action = act.ActivatePaneDirection("Up") },
+	{ key = "l", mods = "LEADER",       action = act.ActivatePaneDirection("Right") },
 
 	-- Resizing
-	{ key = "H",   mods = "LEADER|SHIFT", action = act.AdjustPaneSize { "Left", 5 } },
-	{ key = "J",   mods = "LEADER|SHIFT", action = act.AdjustPaneSize { "Down", 5 } },
-	{ key = "K",   mods = "LEADER|SHIFT", action = act.AdjustPaneSize { "Up", 5 } },
-	{ key = "L",   mods = "LEADER|SHIFT", action = act.AdjustPaneSize { "Right", 5 } },
+	{ key = "H", mods = "LEADER|SHIFT", action = act.AdjustPaneSize { "Left", 5 } },
+	{ key = "J", mods = "LEADER|SHIFT", action = act.AdjustPaneSize { "Down", 5 } },
+	{ key = "K", mods = "LEADER|SHIFT", action = act.AdjustPaneSize { "Up", 5 } },
+	{ key = "L", mods = "LEADER|SHIFT", action = act.AdjustPaneSize { "Right", 5 } },
 
 	-- Tab Navigation (1-9)
-	{ key = "1",   mods = "LEADER",       action = act.ActivateTab(0) },
-	{ key = "2",   mods = "LEADER",       action = act.ActivateTab(1) },
-	{ key = "3",   mods = "LEADER",       action = act.ActivateTab(2) },
-	{ key = "4",   mods = "LEADER",       action = act.ActivateTab(3) },
-	{ key = "5",   mods = "LEADER",       action = act.ActivateTab(4) },
-	{ key = "6",   mods = "LEADER",       action = act.ActivateTab(5) },
-	{ key = "7",   mods = "LEADER",       action = act.ActivateTab(6) },
-	{ key = "8",   mods = "LEADER",       action = act.ActivateTab(7) },
-	{ key = "9",   mods = "LEADER",       action = act.ActivateTab(8) },
+	{ key = "1", mods = "LEADER",       action = act.ActivateTab(0) },
+	{ key = "2", mods = "LEADER",       action = act.ActivateTab(1) },
+	{ key = "3", mods = "LEADER",       action = act.ActivateTab(2) },
+	{ key = "4", mods = "LEADER",       action = act.ActivateTab(3) },
+	{ key = "5", mods = "LEADER",       action = act.ActivateTab(4) },
+	{ key = "6", mods = "LEADER",       action = act.ActivateTab(5) },
+	{ key = "7", mods = "LEADER",       action = act.ActivateTab(6) },
+	{ key = "8", mods = "LEADER",       action = act.ActivateTab(7) },
+	{ key = "9", mods = "LEADER",       action = act.ActivateTab(8) },
 
 	-- Closing
-	{ key = "&",   mods = "LEADER|SHIFT", action = act.CloseCurrentTab { confirm = true } },
-	{ key = "d",   mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
-	{ key = "x",   mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
-}
-
----[ Key Tables
-config.key_tables = {
-
-	-- Triggered by <leader> w
-	window_mode = {
-		{ key = 'v',      action = act.SplitHorizontal { domain = 'CurrentPaneDomain' } },
-		{ key = 's',      action = act.SplitVertical { domain = 'CurrentPaneDomain' } },
-		{ key = 'h',      action = act.ActivatePaneDirection('Left') },
-		{ key = 'j',      action = act.ActivatePaneDirection('Down') },
-		{ key = 'k',      action = act.ActivatePaneDirection('Up') },
-		{ key = 'l',      action = act.ActivatePaneDirection('Right') },
-		{ key = 'q',      action = act.CloseCurrentPane { confirm = true } },
-		{ key = 'c',      action = act.CloseCurrentPane { confirm = true } },
-		{ key = 'H',      action = act.AdjustPaneSize { 'Left', 5 } },
-		{ key = 'J',      action = act.AdjustPaneSize { 'Down', 5 } },
-		{ key = 'K',      action = act.AdjustPaneSize { 'Up', 5 } },
-		{ key = 'L',      action = act.AdjustPaneSize { 'Right', 5 } },
-		{ key = 'Escape', action = 'PopKeyTable' },
-	},
-
-	-- Triggered by <leader> Tab
-	workspace_mode = {
-		{ key = 'n',      action = act.SpawnTab('CurrentPaneDomain') },
-		{ key = 'd',      action = act.CloseCurrentTab { confirm = true } },
-		{ key = '[',      action = act.ActivateTabRelative(-1) },
-		{ key = ']',      action = act.ActivateTabRelative(1) },
-		{ key = 'Escape', action = 'PopKeyTable' },
-	},
+	{ key = "&", mods = "LEADER|SHIFT", action = act.CloseCurrentTab { confirm = true } },
+	{ key = "d", mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
+	{ key = "x", mods = "LEADER",       action = act.CloseCurrentPane { confirm = true } },
 }
 
 config.colors = {
